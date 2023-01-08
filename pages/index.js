a1083ce8bd90333ce9ae35016d1997ca38752b40
Ahmad Mukhtar <ahmedmukhtar1133@gmail.com>
2023-01-09 00:19:23 +0500

    Updated skills and introduction paragraph.

--- pages/index.js
+++ pages/index.js
@@ -91,29 +91,29 @@ export default function Home() {
                                 I am a {' '}
                                 <Box as='span' bgGradient={`linear(to-r, ${useColorModeValue(`brand.600`, `brand.400`)}, ${useColorModeValue(`teal.600`, `teal.400`)}, ${useColorModeValue(`blue.600`, `blue.300`)})`} className='moving-grad' bgClip='text'>FullStack Developer</Box>
                                 {' '} having 6+ years of experience in developing state-of-the-art applications,
-                                building great user experiences by bringing simplicity to life and constantly learning.
+                                building great user experiences. As a passionate web developer - I can develop stable & scalable projects.
                             </chakra.p>
                             <chakra.p maxW='650px' textAlign={{ base: `center`, lg: `left` }} fontSize='xl' mt={2}>
                                 Skilled in technologies like {' '}
                                 <Link href='https://www.electronjs.org/' passHref isExternal>
                                     <ChakraLink>ElectronJS</ChakraLink>
                                 </Link>{', '}
+                                <Link href='https://nextjs.org/' passHref isExternal>
+                                    <ChakraLink>NextJS</ChakraLink>
+                                </Link>{', '}
+                                <Link href='https://www.typescriptlang.org//' passHref isExternal>
+                                    <ChakraLink>TypeScript</ChakraLink>
+                                </Link>{', '}
                                 <Link href='https://reactjs.org/' passHref isExternal>
-                                    <ChakraLink>React js</ChakraLink>
+                                    <ChakraLink>ReactJS</ChakraLink>
                                 </Link>{', '}
                                 <Link href='https://laravel.com/' passHref isExternal>
                                     <ChakraLink>Laravel</ChakraLink>
                                 </Link>{', '}
                                 <Link href='https://nodejs.org/en/' passHref isExternal>
-                                    <ChakraLink>Node</ChakraLink>
-                                </Link>{', '}
-                                <Link href='https://v3.vuejs.org/' passHref isExternal>
-                                    <ChakraLink>Vue js</ChakraLink>
-                                </Link>{', '}
-                                <Link href='https://wordpress.com/' passHref isExternal>
-                                    <ChakraLink>Wordpress</ChakraLink>
+                                    <ChakraLink>NodeJS</ChakraLink>
                                 </Link>{' . '}
-                                I am a team player and always looking for new challenges and ways to improve myself.
+                                Launched multiple projects from the empty repository to fully-functional production deployments. I am a team player and always looking for new challenges and ways to improve myself.
                             </chakra.p>
                         </Flex>
                     </Flex>
